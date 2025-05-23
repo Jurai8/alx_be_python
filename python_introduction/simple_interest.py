3a14bbde6de3053d7f8ac11e5d03dd913c8eb387
Charles <131194505+Jurai8@users.noreply.github.com>
2025-05-23 19:48:04 +0200

    practicing python

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+
+# interest earned
+
+# principle amount (initial investment)
+principal = 1000
+
+# annual interest rate (decimal)
+rate = 0.05
+
+# years that the money has been invested
+time = 3
+
+interest = (principal * rate * time)
+
+print(f"The simple interest is: {interest}")
