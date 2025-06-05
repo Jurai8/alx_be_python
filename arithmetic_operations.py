65afb087cbd2be999a8f32c1191c025df3467fbb
Charles <131194505+Jurai8@users.noreply.github.com>
2025-06-05 20:13:13 +0200

    arithmetic operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,22 @@
+
+
+def perform_operation(num1, num2, operation):
+    answer = 0
+    # match case for each operation
+    match operation:
+        case "add":
+            answer = num1 + num2
+            return answer
+        case "subtract":
+            answer = num1 - num2
+            return answer
+        case "multiply":
+            answer = num1 * num2
+            return answer
+        case "divide":
+            if num1 == 0 or num2 == 0:
+                return "can't divide by zero"
+            else:
+                return num1 / num2         
+        case _:
+            return "No operation"
