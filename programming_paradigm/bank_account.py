c4ab0cf449d4bd279668717c6c4bd495ec11958d
Charles <131194505+Jurai8@users.noreply.github.com>
2025-06-13 21:39:55 +0200

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balane += amount
+
+    def withdraw(self, amount):
+
+        # if insufficient funds, return false
+        if amount > self.account_balance:
+            return False
+        
+        # deduct amount if funds are sufficient
+        else:
+            self.account_balance -= amount
+            return True
+
+
+    def display_balance(self):
+        print(f"Balance: {self.account_balance}")
